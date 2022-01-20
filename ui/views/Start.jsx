b7dcac2fd392a991d4fdf1dc72e05c58b1e1bb4b
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-01-20 08:03:21 +0330

    Changed the new room button text

--- ui/views/Start.jsx
+++ ui/views/Start.jsx
@@ -216,7 +216,7 @@ export default function Start({newRoom = {}, urlRoomId, roomFromURIError}) {
             onClick={submit}
             className="select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
           >
-            🌱 Start room
+            Start a new room
           </button>
         </form>
 
